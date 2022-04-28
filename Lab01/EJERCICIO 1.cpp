8d5cd90052e4c37d3e680370aea316fff9f7122e
Javier Quispe Rojas <103952806+XawiiQR@users.noreply.github.com>
2022-04-27 23:14:59 -0500

    Create EJERCICIO 1.cpp

---
+++ Lab01/EJERCICIO 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  int mult=0;
+  cout<<"ingrese dos numeros enteros: ";
+  cin>>a>>b;
+  mult=a*b;
+  cout<<"la multiplicacion de los 2 numeros enteros es: "<<mult<<endl;
+  return 0;
+}
