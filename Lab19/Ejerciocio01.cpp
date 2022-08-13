db43acde0efc73df757da8f11a0bd5e74030e51b
Javier Quispe Rojas <103952806+XawiiQR@users.noreply.github.com>
2022-08-12 23:59:02 -0500

    Add files via upload

---
+++ Lab19/Ejerciocio01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class RLS{
+private:
+	float xsuma=0;
+	float ysuma=0;
+	float xysuma=0;
+	float x2suma=0;
+public:
+	void operator()(int vect_X [],int vect_Y [],int tamanho){
+		for(int j=0; j<tamanho; j++){
+			xsuma+=vect_X[j];
+			x2suma+=vect_X[j]*vect_X[j];
+			ysuma+=vect_Y[j];
+			xysuma+=vect_X[j]*vect_Y[j];
+		}
+		cout<<"**********Formula: y = a + bx**********"<<endl;
+		float rest1=((tamanho*xysuma)-(xsuma*ysuma));
+		float rest2=((tamanho*x2suma)-(xsuma*xsuma));
+		float m=rest1/rest2;
+		cout<<endl<<"**********Resultado:**********"<<endl;
+		cout<<"m = "<<m<<endl;
+		cout<<"a = "<<(xsuma/tamanho)<<endl;
+		cout<<"b = "<<(ysuma/tamanho)<<endl;
+		
+	}
+};
+int main(int argc, char *argv[]) {
+	int vectX [4]={5,4,3,2};
+	int vectY[4]={9,1,8,2};
+	RLS regresion;
+	regresion(vectX,vectY,4);
+	return 0;
+}
+
---
+++ Lab19/Ejerciocio02.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Find{
+private:
+	vector<int> ind;
+	vector<int> list;
+public:
+	Find(vector<int> lista){
+		this->list=lista;
+	}
+	vector<int> operator()(int start,int end,int dato){
+		for(int i=start;i<end;i++){
+			if(list[i]==dato)ind.push_back(i);
+		}
+		return ind;
+	}
+};
+int main(int argc, char *argv[]) {
+	int dato;
+	int k=0;
+	vector<int>list={4,7,9,3,8,5,3,8,4,9,10,8,3,5,4,7,8};
+	Find p2(list);
+	cout<<"Que numero desea buscar: "<<endl;
+	cin>>dato;
+	for(int j=0; j<list.size();j++){
+		if(list[j]==dato){
+			k++;
+		}
+	}
+	if(k>0){
+		vector<int> ind=p2(0,list.size(),dato);
+		cout<<"El numero "<<dato<<" se encontro en los indices :";
+		for(int i=0;i<ind.size();i++){
+			cout<<ind[i]<<" ";
+		}
+		cout<<endl;
+	}
+	else{
+		cout<<"Dato no existente en la lista"<<endl;
+	}
+	
+	
+	return 0;
+}
+
